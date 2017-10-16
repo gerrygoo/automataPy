b77bf2c4de10cb9c6cdc96204f7bac6782b7bef7
Gerry <gerrygoable@gmail.com>
2017-10-16 17:15:53 -0500

    Git ignore and inital commit

---
+++ automata.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Automata:
+
+    def __init__(self, delta=None, Q=[], sigma = None, F = None):
+        self.Q = set(Q)
+        self.sigma = sigma
+        self.delta = defaultdict(lambda : None) if not delta else delta
+        self.q_0 = None if not q_0 else q_0
+        self.F = F
+        
+
+    def add_state(self, new_state):
+        self.Q.add(new_state)
+        self.delta[new_state] = defaultdict(lambda : None)
+    
+    def add_transition(self, from_state, with_value, to_state):
+        self.delta[from_state][with_value] = to_state 
