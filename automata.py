e5c43493e0e969a53c35940842dc88981f3540e3
Gerry <gerrygoable@gmail.com>
2017-10-16 17:23:57 -0500

    fix the fix

--- automata.py
+++ automata.py
@@ -2,16 +2,16 @@ from collections import defaultdict
 
 class Automata:
 
-    def __init__(self, Q=[], sigma = None, delta=None, q_0=None, F = None):
-        self.Q = set(Q)
+    def __init__(self, Q=set(), sigma=set(), delta=defaultdict(lambda : None), q_0=None, F = set()):
+        self.Q = Q
         self.sigma = sigma
-        self.delta = defaultdict(lambda : None) if not delta else delta
-        self.q_0 = None if not q_0 else q_0
+        self.delta = delta
+        self.q_0 = q_0
         self.F = F
         
 
     def add_state(self, new_state):
-        self.Q.add(new_state)
+        # self.Q.add(new_state)
         self.delta[new_state] = defaultdict(lambda : None)
     
     def add_transition(self, from_state, with_value, to_state):
