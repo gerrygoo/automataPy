3f63dc353b966980cbeaa6ced1802d41f259a7db
Andres de Lago <delagoandres@gmail.com>
2017-10-17 11:12:46 -0500

    Changes IO to automata

--- automata.py
+++ automata.py
@@ -1,17 +1,32 @@
-from collections import defaultdict
+import xmltodict
+
 
 class State:
     last_id = 0
 
-    def __init__(self, name='', initial=False, final=False, id=-1):
+    def __init__(self, name='', initial=False, final=False, id=-1, transitions={}):
         if id == -1:
             self.id = State.last_id
             State.last_id += 1
         else:
-            self.id = id
+            self.id = int(id)
         self.name = 'q{}'.format(self.id) if name == '' else name
         self.initial = initial
         self.final = final
+        self.transitions = transitions
+
+    def addTransition(self, read, to):
+        self.transitions[read] = to
+
+    def to_dict(self):
+        res = {'@id': self.id, '@name': self.name}
+        if self.initial:
+            res['initial'] = None
+        if self.final:
+            res['final'] = None
+
+    def transitionList(self):
+        return [{'from': self.id, 'to': self.transitions[transition].id, 'read': transition} for transition in self.transitions]
 
     def __hash__(self):
         return self.id.__hash__()
@@ -23,36 +38,48 @@ class State:
         return self.id == other.id
 
 
-class Transition:
+class Automaton:
 
-    def __init__(self, _from, to, read):
-        self._from = _from
-        self.to = to
-        self.read = read
+    @staticmethod
+    def fromJFLAP(filename):
+        """
+            Returns states, transitions
+        """
+        try:
+            with open(filename) as file:
+                jflapDict = xmltodict.parse(file.read())
+                states = {}
+                for state in jflapDict['structure']['automaton']['state']:
+                    obj = State(id=state['@id'], name=state['@name'],
+                                initial=('initial' in state), final=('final' in state))
+                    states[obj.id] = obj
+                for transition in jflapDict['structure']['automaton']['transition']:
+                    id = int(transition['from'])
+                    states[id].addTransition(
+                        transition['read'], states[int(transition['to'])])
+                return Automaton(set(states.values()))
+        except OSError:
+            print("The file: '{}' couln't be opened".format(filename))
 
-    def __eq__(self, other):
-        return self._from == other._from and self.to == other.to and self.read == other.read
+    def toJFLAP(self, filename):
+        jflapDict = {'structure': {'type': 'fa', 'automaton': {
+            'state': [state.to_dict() for state in self.states],
+            'transition': [transition for state in self.states for transition in state.transitionList()]}}}
+        try:
+            with open(filename, 'w') as file:
+                file.write(xmltodict.unparse(jflapDict, pretty=True))
+        except OSError:
+            print("The file: '{}' couln't be opened or created".format(filename))
 
-    def __hash__(self):
-        return hash((self.to+self._from+self.read))
-
-
-class Automata:
-
-    def __init__(self, Q=[], sigma=[], delta={}, q_0=None, F = []):
-        self.Q = set(Q)
-        self.sigma = set(sigma)
-        self.delta = defaultdict(None, delta)
-        self.q_0 = q_0
-        self.F = set(F)
-        
+    def __init__(self, states=[], initial=None):
+        self.states = states
+        self.initial = initial
 
     def add_state(self, new_state):
-        self.Q.add(new_state)
-        self.delta[new_state] = defaultdict(set)
-    
+        self.states[new_state.id] = new_state
+
     def add_transition(self, from_state, with_value, to_state):
-            self.delta[from_state][with_value].add(to_state)
+        self.states[from_state].addTransition(with_value, to_state)
 
     def dfa_transform(self):
         def p(s):
@@ -62,18 +89,18 @@ class Automata:
             return result
 
         deterministic = {
-            'Q':p(self.Q), 
-            'sigma':self.sigma,
-            'delta':None,
-            'q_0':self.q_0,
-            'F':None
+            'Q': p(self.Q),
+            'sigma': self.sigma,
+            'delta': None,
+            'q_0': self.q_0,
+            'F': None
         }
 
         # determinisic sigma
-        delta = {} #from state in P(Q):list to set of states given a key
+        delta = {}  # from state in P(Q):list to set of states given a key
         # delta[from_tuple][key] = to_set
 
-        for state_list in deterministic['Q'][1:]: 
+        for state_list in deterministic['Q'][1:]:
             delta[tuple(state_list)] = {}
 
             for key in self.sigma:
@@ -83,20 +110,16 @@ class Automata:
                     to_set |= self.delta[ind_state][key]
 
                 delta[tuple(state_list)][key] = to_set
-            
 
         deterministic['delta'] = delta
 
         # deterministic F
         F = []
-        for state_list in deterministic['Q']: 
+        for state_list in deterministic['Q']:
             for ac in self.F:
                 if ac in state_list:
                     F.append(state_list)
                     break
-                
+
         deterministic['F'] = F
         return deterministic
-
-
-    
