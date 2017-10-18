9862996ab928473727e0093039011fac67d5b51e
Andres de Lago <delagoandres@gmail.com>
2017-10-17 21:53:04 -0500

    Adds toLatex functionality

--- automata.py
+++ automata.py
@@ -1,4 +1,6 @@
 import xmltodict
+import re
+from collections import Set
 
 
 class State:
@@ -40,7 +42,7 @@ class State:
 
     def __str__(self):
         return "\nid: {}\nname:{}\ninitial: {}\nfinal: {}\ntransitions: [\n\t{}\n]".format(self.id, self.name, self.initial, self.final,
-                                                                                  "\n\t".join(["to: {}, read: {}".format(self.transitions[x].id, x) for x in self.transitions]))
+                                                                                           "\n\t".join(["to: {}, read: {}".format(self.transitions[x].id, x) for x in self.transitions]))
 
     __repr__ = __str__
 
@@ -64,26 +66,80 @@ class Automaton:
                     id = int(transition['from'])
                     states[id].addTransition(
                         transition['read'], states[int(transition['to'])])
-                return Automaton(set(states.values()))
+                return Automaton(states)
         except OSError:
             print("The file: '{}' couln't be opened".format(filename))
 
     def toJFLAP(self, filename):
         jflapDict = {'structure': {'type': 'fa', 'automaton': {
-            'state': [state.to_dict() for state in self.states],
-            'transition': [transition for state in self.states for transition in state.transitionList()]}}}
+            'state': [state.to_dict() for state in self.states.values()],
+            'transition': [transition for state in self.states.values() for transition in state.transitionList()]}}}
         try:
             with open(filename, 'w') as file:
                 file.write(xmltodict.unparse(jflapDict, pretty=True))
         except OSError:
             print("The file: '{}' couln't be opened or created".format(filename))
 
-    def __init__(self, states={}, initial=None):
-        self.states = states
-        self.initial = initial
+    def toLatex(self):
+        def underscore(st):
+            return re.sub(r'(?<=\w)(\d+)', r'_{\1}', st)
+        print('\\begin{enumerate}')
+        stt = underscore(', '.join([str(x.name)
+                                    for x in self.states.values()]))
+        ident = '  '
+        print(ident + '\\item $Q = \\{' + stt + '\\}$')
+        reads = set()
+        for st in self.states.values():
+            for tt in st.transitions:
+                reads.add(tt)
+        reads = sorted(list(reads))
+        stt = ", ".join([str(x) for x in reads])
+        print(ident + '\\item $\\Sigma = \\{' + stt + '\\}$')
+        print(ident + '\\item $q_0$ = $' +
+              underscore(self.initial.name) + ' \\in Q$')
+        print(ident + '\\item $F = \{' + ', '.join(
+            [underscore(x.name) for x in self.states.values() if x.final]) + '\}$')
+        print(ident + '\\item $\\delta \\colon Q \\times \\Sigma \\rightarrow Q = $')
+        print(ident + '\\begin{tabular}{*{' + str(len(reads) + 1) + '}{c|}}')
+        ident += '  '
+        print(ident + '& $ ' +
+              ' $ & $ '.join([str(x) for x in reads]) + ' $ \\\\')
+        print(ident + '\\hline')
+        ordered_states = []
+
+        def number_on_string(a):
+            n = re.sub(r'\D+', '', a.name)
+            return int(n) if n != '' else 0
+
+        if re.search(r'(\d+)', self.states[0].name):
+            ordered_states = sorted(self.states.values(), key=number_on_string)
+        elif self.states[0].name.strip()[0] == '{':
+            ordered_states = sorted(
+                self.states.values(), key=lambda a: a.name.count(','))
+        else:
+            ordered_states = sorted(self.states.values(), key=lambda a: a.name)
+
+        for state in ordered_states:
+            print(ident + '$ ' + underscore(state.name) + ' $ & $ ' + ' $ & $ '.join([underscore(
+                state.transitions[x].name) if x in state.transitions else ' ' for x in reads]) + ' $\\\\\n' + ident + '\\hline')
+        ident = '  '
+        print(ident + '\\end{tabular}')
+        print('\\end{enumerate}')
+
+    def __init__(self, states=None, initial=None):
+        self.states = states if states != None else {}
+        if initial != None:
+            self.initial = initial
+        else:
+            for st in self.states.values():
+                if st.initial:
+                    self.initial = st
+                    break
 
     def add_state(self, new_state):
         self.states[new_state] = new_state
+        if new_state.initial:
+            self.initial = new_state
 
     def add_transition(self, from_state, with_value, to_state):
         self.states[from_state].addTransition(with_value, to_state)
--- test.py
+++ test.py
@@ -22,3 +22,4 @@
 from automata import *
 aut = Automaton.fromJFLAP('1.jff.in')
 aut.toJFLAP('1.jff.out')
+aut.toLatex()
