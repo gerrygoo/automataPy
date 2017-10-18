131f6f136b24607beb5a8b3591b3cb1700dff700
Andres de Lago <delagoandres@gmail.com>
2017-10-18 15:55:25 -0500

    Adds simple

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+# from automata import *
+
+# aut = Automaton.fromJFLAP('1.jff.in')
+# aut.toJFLAP('1.jff.out')
+# print(aut.toLatex())
+
 from automata import *
 
-aut = Automaton.fromJFLAP('1.jff.in')
-aut.toJFLAP('1.jff.out')
-print(aut.toLatex())
+aut = Automaton.fromJFLAP('simple.jff')
+new = aut.dfa_transform()
+new.toJFLAP('simple.jff.out')
+print(new.toLatex())
